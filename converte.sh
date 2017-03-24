bea9466905fd786dcb135bc90adeaf7037a4b1e5
Bruno Vieira Costa <brunopsycon@gmail.com>
2017-03-23 22:53:04 -0300

    json

--- converte.sh
+++ converte.sh
@@ -3,13 +3,13 @@ npm install
 for file in $( ls ./CVs/XMLNOVO | sed "s/\\..*//g"); do
 node <<EOF
 fs = require("fs");
-p = require("xml2json");
+parser = require("xml2json");
 fs.readFile("./CVs/XMLNOVO/$file.xml","utf-8",function(err,data){
 	if(err){
 		console.log("erro ao converter $file");
 		console.log(err);
 	}else{
-		fs.writeFile("./CVs/JSON/$file.json",data,function(){
+		fs.writeFile("./CVs/JSON/$file.json",parser.toJson(data),function(){
 			console.log("arquivo $file convertido com sucesso!");
 		})
 	}
