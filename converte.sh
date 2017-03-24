fc9ee4a6f5fd8f43a4aeae4043d14830b04d0db3
Bruno Vieira Costa <brunopsycon@gmail.com>
2017-03-23 23:48:40 -0300

    generate json with _on attrs

--- converte.sh
+++ converte.sh
@@ -2,15 +2,22 @@
 npm install
 for file in $( ls ./CVs/XMLNOVO | sed "s/\\..*//g"); do
 node <<EOF
-fs = require("fs");
-parser = require("xml2json");
+var fs = require("fs");
+var util = require("util");
+var parser = require("xml2js").parseString;
+
 fs.readFile("./CVs/XMLNOVO/$file.xml","utf-8",function(err,data){
 	if(err){
 		console.log("erro ao converter $file");
 		console.log(err);
 	}else{
-		fs.writeFile("./CVs/JSON/$file.json",parser.toJson(data),function(){
-			console.log("arquivo $file convertido com sucesso!");
+			parser(data,{mergeAttrs: true,attrNameProcessors: [function(name){
+				return "_"+name;
+			}]},function(err,json){
+			fs.writeFile("./CVs/JSON/$file.json",JSON.stringify(json),function(){
+				console.log(util.inspect(err));
+				console.log("arquivo $file convertido com sucesso!");
+			})
 		})
 	}
 });
