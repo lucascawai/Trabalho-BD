941e5523a9c49cdaba779418055314b051bbe01c
Bruno Vieira Costa <brunopsycon@gmail.com>
2017-03-24 00:59:33 -0300

    Foi

--- converte.sh
+++ converte.sh
@@ -4,20 +4,15 @@ for file in $( ls ./CVs/XMLNOVO | sed "s/\\..*//g"); do
 node <<EOF
 var fs = require("fs");
 var util = require("util");
-var parser = require("xml2js").parseString;
+var parser = require("xml2json")
 
 fs.readFile("./CVs/XMLNOVO/$file.xml","utf-8",function(err,data){
 	if(err){
 		console.log("erro ao converter $file");
 		console.log(err);
 	}else{
-			parser(data,{mergeAttrs: true,attrNameProcessors: [function(name){
-				return "_"+name;
-			}]},function(err,json){
-			fs.writeFile("./CVs/JSON/$file.json",JSON.stringify(json),function(){
-				console.log(util.inspect(err));
-				console.log("arquivo $file convertido com sucesso!");
-			})
+		fs.writeFile("./CVs/JSON/$file.json",parser.toJson(data),function(){
+			console.log("arquivo $file convertido com sucesso!");
 		})
 	}
 });
--- mongodb_queries.js
+++ mongodb_queries.js
@@ -4,26 +4,26 @@ function get_results(result) {
 
 print("########################################")
 print("1) Pessoas que falam francês: ")
-db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.IDIOMAS.IDIOMA._IDIOMA':"FR"}, {'CURRICULO-VITAE.DADOS-GERAIS._NOME-COMPLETO':1, "_id":0}).forEach(get_results)
+db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.IDIOMAS.IDIOMA.IDIOMA':"FR"}, {'CURRICULO-VITAE.DADOS-GERAIS.NOME-COMPLETO':1, "_id":0}).forEach(get_results)
 print("########################################")
 
 print("########################################")
 print("2) Pessoas que possuem mestrado: ")
-db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.FORMACAO-ACADEMICA-TITULACAO.MESTRADO':{$exists:true}}, {'CURRICULO-VITAE.DADOS-GERAIS._NOME-COMPLETO':1, "_id":0}).forEach(get_results)
+db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.FORMACAO-ACADEMICA-TITULACAO.MESTRADO':{$exists:true}}, {'CURRICULO-VITAE.DADOS-GERAIS.NOME-COMPLETO':1, "_id":0}).forEach(get_results)
 print("########################################")
 
 print("########################################")
 print("3) Quantas pessoas de cada estado: ")
-db.curriculos.group({key:{'CURRICULO-VITAE.DADOS-GERAIS._UF-NASCIMENTO':1}, reduce: function(curr, result){result.total += 1}, initial: {total: 0}}).forEach(get_results)
+db.curriculos.group({key:{'CURRICULO-VITAE.DADOS-GERAIS.UF-NASCIMENTO':1}, reduce: function(curr, result){result.total += 1}, initial: {total: 0}}).forEach(get_results)
 print("########################################")
 
 
 print("########################################")
 print("4a) Pessoas e seus prêmios: (sem unwind)")
-db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS':{$exists:true}}, {'CURRICULO-VITAE.DADOS-GERAIS._NOME-COMPLETO':1, "_id":0, "CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO":1}).forEach(get_results)
+db.curriculos.find({'CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS':{$exists:true}}, {'CURRICULO-VITAE.DADOS-GERAIS.NOME-COMPLETO':1, "_id":0, "CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO":1}).forEach(get_results)
 print("########################################")
 
 print("########################################")
 print("4b) Pessoas e seus prêmios: (com unwind)")
-db.curriculos.aggregate([{$unwind: '$CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO' },{$project:{"_id":1, "CURRICULO-VITAE.DADOS-GERAIS._NOME-COMPLETO":1, "CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO":1}}]).forEach(get_results)
+db.curriculos.aggregate([{$unwind: '$CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO' },{$project:{"_id":1, "CURRICULO-VITAE.DADOS-GERAIS.NOME-COMPLETO":1, "CURRICULO-VITAE.DADOS-GERAIS.PREMIOS-TITULOS.PREMIO-TITULO":1}}]).forEach(get_results)
 print("########################################")
